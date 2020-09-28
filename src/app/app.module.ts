848389878a38235e902af2cb8c58c19e29bf38ae
Prathaam-Modi <pm030800@gmail.com>
2020-09-28 16:51:42 +0545

    SL lab todo components and directives update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TodoComponent } from './components/todo.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoComponent } from './components/todo/todo.component';
+import { TodoHeaderComponent } from './todo/todo-header/todo-header.component';
+import { TodoInputComponent } from './todo/todo-input/todo-input.component';
+import { TodoFooterComponent } from './todo/todo-footer/todo-footer.component';
+import { TodoListsComponent } from './todo/todo-lists/todo-lists.component';
 @NgModule({
   declarations: [
     AppComponent,
     TodoComponent,
-    TodoListComponent
+    TodoHeaderComponent,
+    TodoInputComponent,
+    TodoFooterComponent,
+    TodoListsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/todo/todo-lists/todo-lists.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-lists',
+  templateUrl: './todo-lists.component.html',
+  styleUrls: ['./todo-lists.component.css']
+})
+export class TodoListsComponent {
+
+  constructor() { }
+
+  listOfplace = ['Paris','Santiorini','Bali','Ibiza']
+
+}
